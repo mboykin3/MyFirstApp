0d5c7f1a10c2871bfc78d66f1a463a39d5bddbca
mboykin3 <47874862+mboykin3@users.noreply.github.com>
2019-03-07 11:17:56 -0500

    "main.js loaded"

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-let myNumber = 10;
+(function() {
+  'use strict' 
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  // var c;
+  for(var x25; x<400; x+=50 {
+      for(var y=25; y<400; y+=50) {
+          c = Shape.Circle(x, y, 20);
+          c.fillColor = 'green';
+      }
+  })
+
+  paper.view.draw();
+  // your code will start here...
+
+console.log('main.js loaded');
+
+  // ...and end here 
+}())
